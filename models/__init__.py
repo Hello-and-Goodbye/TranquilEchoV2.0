91517abd40fa53b4b73ae0f47fbbdf1cce680831
哪里来的小趴菜 <3579960880@qq.com>
2024-03-25 23:27:19 +0800

    对model下的__init__.py和main.py做了写完善

--- models/__init__.py
+++ models/__init__.py
@@ -12,11 +12,39 @@ class Therapist(BaseModel):
     phone_number:str
     title:str
     description:str
+    age:int
+    qualification:str
+    is_active:bool
+    cases:str
+    history:str
+    gender:str
 class Message(BaseModel):
     content:str
+    history:str
+    date:str
+    message_type:str
+    user_id:int
+    thread_id:int
+    id:int
+    keywords:str
 class User(BaseModel):
     username:str
     password:str
     email:str
     full_name:str
     phone_number:str
+    description:str
+    history:str
+    social_media:str
+    is_active:bool
+    age:int  
+    gender:str
+    is_staff:bool
+    job_title:str
+    is_superuser:bool
+    level:int
+    index:int
+
+
+
+
